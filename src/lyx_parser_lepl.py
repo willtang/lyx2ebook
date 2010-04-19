e643e3d5874176c3185121319279557cf9d9c14f
willtang <whitmenn@gmail.com>
2010-04-19 20:33:39 +0800

    Completed basic LyX file parsing.

--- src/lyx_parser_lepl.py
+++ src/lyx_parser_lepl.py
@@ -10,16 +10,41 @@ from lepl import *
 #    query.config.auto_memoize(full=True)
 #    
 #    print query.parse('spicy meatballs OR "el bulli restaurant"')
-#
 
-document = Literal('\\begin_document') & ~Newline() & UnsignedInteger() & ~Newline() & Literal('\\end_document') & ~Newline()
-root = document | (Literal('\\lyxformat')  & ~Space() & UnsignedInteger() & ~Newline())
-lyx = root[:]
+def parse(file):
+    # Match one or more new line
+    newlines = ~Newline()[1:]
+    
+    slash = ~Literal('\\')
+    
+    # Match sentence
+    sentence = Word() & (Space() & Word())[:] & Space()[:] > "".join
+    
+    # Match comment which starts a new line with #
+    comment = Literal('#') & AnyBut("\n\r")[:] & newlines
+    
+    # Match command in the format of "\XXX YYY ZZZ ..."
+    command = slash & sentence & newlines > "".join
+    
+    # Main LyX document definition
+    layout = slash & Literal('begin_layout') & newlines & (command | (sentence & newlines))[:] & slash & ~Literal('end_layout') & newlines > list
+    body = slash & Literal('begin_body') & newlines & (command | (sentence & newlines))[:] & slash & ~Literal('end_body') & newlines > list
+    header = slash & Literal('begin_header') & newlines & command[:] & slash & ~Literal('end_header') & newlines > list
+    document = slash & Literal('begin_document') & newlines & header & body & slash & ~Literal('end_document') & newlines > list
+    root = document | command | ~comment
+    lyx = root[:]
+    
+    # Read LyX document
+    f = open(file, 'r')
+    
+    # Parse the LyX document
+    result = lyx.parse(f.read())
+    
+    # Close the opened LyX document
+    f.close()
+    
+    return result
 
-f = open('test2.lyx', 'r')
-
-result = lyx.parse(f.read())
-
-f.close()
-
-print result
+if __name__ == "__main__":
+    
+    print parse('novel.lyx')
